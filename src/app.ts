4f8e99789c0655f74c7246dfd9d512a75289d667
AlliePinnell <x-rap@hotmail.ca>
2026-02-01 19:56:55 -0500

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser'; // accept json body in POST / PUT requests
+import swaggerJsDoc from 'swagger-jsdoc'; // api doc generator
+import swaggerUi from 'swagger-ui-express';
+import mongoose from 'mongoose';  // mongodb access lib
+
+// controllers
+import parks from './controllers/parks';
+
+const app: Application = express();
+
+// configure app globally to parse http request bodies as json
+app.use(bodyParser.json());
+
+// db connection
+const dbUri = process.env.DB!;
+
+mongoose.connect(dbUri)
+.then(() => { console.log('Connected to MongoDB') })
+.catch((err: Error) => { console.log(`Connection Failed: ${err.message}`) });
+
+// url dispatching
+app.use('/api/v1/parks', parks);
+
+// swagger api doc config
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Particular Parks API',
+            version: '1.0.0'
+        }
+    },
+    apis: ['./dist/controllers/*.js'] // location of api methods
+}
+
+const openApiSpecs = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUi.serve);
+
+// hard-code swagger css & js links using public Content Delivery Network (CDN)
+app.get('/api-docs', (req: Request, res: Response) => {
+    const html: string = swaggerUi.generateHTML(openApiSpecs, {
+        customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+        customJs: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
+        ]
+    });
+
+    res.send(html);
+});
+
+app.listen(4000, () => { console.log('Server running on port 4000') });
